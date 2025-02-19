0f75d41036be831f5d78abad9c66b0751256c832
Nur Salim <nursalim210302@gmail.com>
2025-02-19 15:22:24 +0700

    Menghapus folder yang tidak diperlukan

--- Pachkage/WebCopy/wcopycmd.cmd
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-FOR /f "tokens=2*" %%A in ('REG QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\App Paths\wcopy.exe" /v "Path"') DO SET CYOWCCMD=%%B
-
-SET "PATH=%CYOWCCMD%;%PATH%"
-
-WCOPY.exe
